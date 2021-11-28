18db87d2a180e6f5a09c22a06b8ca4a5c3695e1a
linyi <1158029271@qq.com>
2021-11-28 11:03:45 +0800

    roomID,RoomId->roomId

--- src/main/java/com/example/util/PeopleRecordUtil.java
+++ src/main/java/com/example/util/PeopleRecordUtil.java
@@ -11,34 +11,45 @@ import org.springframework.web.socket.WebSocketSession;
 
 public class PeopleRecordUtil {
     private final int roomCnt = 10;
-    private ArrayList<ArrayList<WebSocketSession>> clientsOfRoom = new ArrayList<>();
+    private ArrayList<ArrayList<SessionEntity>> clientsOfRoom = new ArrayList<>();
     private static final Logger logger = LoggerFactory.getLogger(RoomHandler.class);
 
     // 必须要为每个元素指定一个空间
     public PeopleRecordUtil() {
         for (int i = 0; i < roomCnt; ++i) {
-            ArrayList<WebSocketSession> elem = new ArrayList<>();
+            ArrayList<SessionEntity> elem = new ArrayList<>();
             clientsOfRoom.add(elem);
         }
     }
 
     public void addPeople(WebSocketSession session, int roomId) {
-        logger.info("⏩进入自习室" + roomId + "：session=" + session);
-        clientsOfRoom.get(roomId).add(session);
+        logger.info("⏩进入自习室" + roomId + "：sessionId=" + session.getId());
+        SessionEntity sessionEntity = new SessionEntity(session, session.getId());
+        clientsOfRoom.get(roomId).add(sessionEntity);
     }
 
     public void deletePeople(WebSocketSession session, int roomId) {
-        logger.info("⏪退出自习室" + roomId + "：session=" + session);
-        clientsOfRoom.get(roomId).remove(session);
+        logger.info("⏪退出自习室" + roomId + "：session=" + session.getId());
+        // clientsOfRoom.get(roomId).remove(sessionEntity);
+        for (int i = 0; i < clientsOfRoom.get(roomId).size(); ++i) {
+            SessionEntity sessionEntity = clientsOfRoom.get(roomId).get(i);
+            if (sessionEntity.getSessionId().equals(session.getId())) { // 只判断id
+                clientsOfRoom.get(roomId).remove(i); // 直接根据索引去除该元素
+                break;
+            }
+        }
     }
 
     // 把新人数推送给该自习室中的所有客户端
     public void pushClient(int roomId) throws Exception {
         logger.info("📢推送目标：");
-        ArrayList<WebSocketSession> arrayList = clientsOfRoom.get(roomId);
+        ArrayList<SessionEntity> arrayList = clientsOfRoom.get(roomId);
 
-        for (WebSocketSession session : arrayList) {
-            logger.info("session=" + session);
+        for (SessionEntity sessionEntity : arrayList) {
+            String sessionId = sessionEntity.getSessionId();
+            logger.info("sessionId=" + sessionId);
+            WebSocketSession session = sessionEntity.getSession();
+            // 因为还需要向session发送消息，所以不能简单把session全部换成sessionId
             session.sendMessage(new TextMessage("" + clientsOfRoom.get(roomId).size()));
         }
     }
